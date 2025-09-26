a62a5a4b59e1a7b08809514d33de2779cf6e3066
wrnev <108156561+wrnev@users.noreply.github.com>
2025-09-26 01:19:56 -0400

    fixes bait n switch to be to the rickroll, not repo

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 // Shorthands referencing to the link
 let hyperlink = document.getElementById("hyperlink");
 
-// Replaced enable js link now that js is enabled
-hyperlink.setAttribute("href","https://youtu.be/dQw4w9WgXcQ");
+// Replaced enable js link now that js is enabled, showing the repo in the link preview
+hyperlink.setAttribute("href","https://github.com/wrnev/placeholder.fyi");
 
 function linkClicked(event) {
     // Comment when the victim on the rickroll
     console.log("Haha, you clicked!");
 
-    // Changes the lable and direction of the link to the actual repository
-    hyperlink.innerHTML = "okay, here's the real repository"
-    hyperlink.setAttribute("href","https://github.com/wrnev/placeholder.fyi");
+    // Changes the lable and direction of the link to the rickroll before the interface can show it
+    hyperlink.setAttribute("href","https://youtu.be/dQw4w9WgXcQ");
+
+    // Sets the link to be normal after this interaction
+    setTimeout(function() {
+        hyperlink.innerHTML = "Okay, heres the real repository";
+        hyperlink.setAttribute("href","https://github.com/wrnev/placeholder.fyi");
+        hyperlink.setAttribute("id","");
+    }, 200); 
   }
